0f0bec871651660d9b9783a4425cac48b27aa64c
abdullah-havelent <abdullah.tariq.ci@gmail.com>
2026-09-24 09:17:31 +0500

    Add all public pages to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,17 +5,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   const paths = [
     '',
-    '/founder',
+    '/reviews',
     '/our-work',
     '/our-work/designs',
     '/our-work/social-performance',
     '/our-work/campaign-results',
-    '/blog',
     '/services/video-editing',
     '/services/graphic-design',
     '/services/digital-marketing',
     '/services/social-media-management',
     '/services/web-development',
+    '/about',
+    '/founder',
+    '/blog',
+    '/blog/video-editing',
+    '/blog/graphic-design',
+    '/blog/digital-marketing',
+    '/blog/social-media-management',
+    '/blog/web-development',
+    '/contact',
   ];
 
   return paths.map((path) => ({
